a8f069181822d53e7979edc1a5622f5fe6b3e4c2
Masa Abushamleh <36239840+nerdingitout@users.noreply.github.com>
2022-09-07 19:57:47 +0400

    Update hello_function.js

--- src/hello_function.js
+++ src/hello_function.js
@@ -1,3 +1,4 @@
+[sourcecode language=”javascript”]
 /**
 *
 * main() will be run when you invoke this action
@@ -7,9 +8,7 @@
 * @return The output of this action, which must be a JSON object.
 *
 */
-‘use strict’;
 function main(params) {
-console.log(‘Hello function called with: ‘ + JSON.stringify(params));
 var greeting = params.greeting;
 var user = params.user;
 var message = greeting + ", " + user;
